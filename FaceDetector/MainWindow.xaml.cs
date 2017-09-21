716d1602d6c08f3969cfe5c31e5ea1fb5155cd41
Ledrunner <ledrunner@hotmail.com>
2017-09-21 17:00:59 +0300

    Added Image converter class, Added open.cv dll's and code for camera capture

--- FaceDetector/MainWindow.xaml.cs
+++ FaceDetector/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using FaceDetector.Service;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FaceDetector
 {
@@ -20,11 +11,74 @@ namespace FaceDetector
     /// </summary>
     public partial class MainWindow : Window
     {
+        private VideoCapture _capture = null;
+        private bool _captureInProgress;
+        private Mat _frame;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            // Инит EMGUU 
+
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new VideoCapture();
+                // Подписываемся на событие
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+           
+            _frame = new Mat();
+           
         }
 
-       
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+
+                //var temp = _frame.ToImage<Bgr, byte>().Bitmap;
+  
+                imageBox.Image = _frame;
+
+                //  imageBox.Source = BitmapSourceConvert.ToBitmapSource(temp as Image);
+            }
+        }
+
+        private void captureButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    captureButton.Content = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    captureButton.Content = "Stop";
+
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (_capture != null)
+                _capture.Dispose();
+        }
+
+     
     }
 }
