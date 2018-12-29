e2f6722cada44251bbd2927d990b7b56ec3537d2
Ledrunning <ledrunner@hotmail.com>
2018-12-29 14:08:25 +0300

    MVVM pattern body has added. ICommand and INotifyPropertyChanged implemented.

---
+++ FaceDetector/Helpers/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FaceDetector.Helpers
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _command;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public DelegateCommand(Action command, Func<bool> canExecute = null)
+        {
+            _canExecute = canExecute;
+            _command = command ?? throw new ArgumentNullException();
+        }
+
+        public void Execute(object parameter)
+        {
+            _command();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+    }
+}
---
+++ FaceDetector/Helpers/NotificationObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FaceDetector.Helpers
+{
+    public abstract class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+    }
+}
