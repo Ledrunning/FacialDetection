dd7608cb12d861779996d19afad22644fd39a455
Ledrunning <ledrunner@hotmail.com>
2020-09-28 21:14:56 +0300

    Deleted unused code in RelayCommand

--- CameraCapture/Helpers/RelayCommand.cs
+++ CameraCapture/Helpers/RelayCommand.cs
@@ -41,48 +41,4 @@ namespace CameraCaptureWPF.Helpers
             CanExecuteChanged(this, EventArgs.Empty);
         }
     }
-
-    public class RelayCommand<T> : ICommand
-    {
-        private readonly Func<T, bool> targetCanExecuteMethod;
-        private readonly Action<T> targetExecuteMethod;
-
-        public RelayCommand(Action<T> executeMethod)
-        {
-            targetExecuteMethod = executeMethod;
-        }
-
-        public RelayCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
-        {
-            targetExecuteMethod = executeMethod;
-            targetCanExecuteMethod = canExecuteMethod;
-        }
-
-        bool ICommand.CanExecute(object parameter)
-        {
-            if (targetCanExecuteMethod != null)
-            {
-                var tparm = (T) parameter;
-                return targetCanExecuteMethod(tparm);
-            }
-
-            if (targetExecuteMethod != null) return true;
-            return false;
-        }
-
-        // Beware - should use weak references if command instance lifetime is longer than lifetime of
-        // UI objects that get hooked up to command
-        // Prism commands solve this in their implementation
-        public event EventHandler CanExecuteChanged = (sender, args) => { };
-
-        void ICommand.Execute(object parameter)
-        {
-            targetExecuteMethod?.Invoke((T) parameter);
-        }
-
-        public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged(this, EventArgs.Empty);
-        }
-    }
 }
