ad6e7f373fc2fe9f64165374f8fad458a55741b2
Ledrunning <ledrunner@hotmail.com>
2022-12-01 09:56:50 +0100

    Removed video player

--- CameraCapture/ViewModel/BaseViewModel.cs
+++ CameraCapture/ViewModel/BaseViewModel.cs
@@ -1,8 +1,27 @@
-﻿using CVCapturePanel.Helpers;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CVCapturePanel.ViewModel
 {
-    public class BaseViewModel : NotificationObject
+    public abstract class BaseViewModel
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return;
+            }
+
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
     }
 }
--- CameraCapture/ViewModel/MainViewModel.cs
+++ CameraCapture/ViewModel/MainViewModel.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Data;
 using System.Windows.Input;
-using CVCapturePanel.Helpers;
-using CVCapturePanel.Model;
 using CVCapturePanel.Service;
 using Emgu.CV;
 using Emgu.CV.Structure;
@@ -14,16 +12,9 @@ namespace CVCapturePanel.ViewModel
     public class MainViewModel : BaseViewModel
     {
         public delegate void ImageWithDetectionChangedEventHandler(object sender, Image<Bgr, byte> image);
-
-        private readonly IDialogService dialog = new DialogService();
-        private readonly IList<VideoSource> sourceList = new List<VideoSource>();
         private string buttonContent = "Start";
         private Bitmap frame;
-
         private bool isStreaming;
-        private string selectedVideoSource;
-
-        private VideoPlayingService videoPlayingService;
         private WebCamService webCamService;
 
         /// <summary>
@@ -31,9 +22,7 @@ namespace CVCapturePanel.ViewModel
         /// </summary>
         public MainViewModel(Action methodAction)
         {
-            InitializeServices();
             InitializeCommands();
-            FillComboBox();
             CloseAction = methodAction;
         }
 
@@ -42,17 +31,10 @@ namespace CVCapturePanel.ViewModel
         /// </summary>
         public ICommand ToggleWebServiceCommand { get; set; }
 
-        public ICommand ToogleOpenVideoCommand { get; set; }
+        
+        public ICommand ToggleCloseAppCommand { get; set; }
 
-        public ICommand ToogleCloseAppCommand { get; set; }
-
-        public CollectionView Video { get; private set; }
         public Action CloseAction { get; set; }
-        public string OpenSource { get; } = "Open video";
-        public string CloseSource { get; } = "Close video";
-        public string Exit { get; } = "Exit";
-
-        public string SelectSource { get; set; } = "Select source";
 
         public string ButtonContent
         {
@@ -60,21 +42,6 @@ namespace CVCapturePanel.ViewModel
             set
             {
                 buttonContent = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string SelectedVideoSource
-        {
-            get => selectedVideoSource;
-            set
-            {
-                if (selectedVideoSource == value)
-                {
-                    return;
-                }
-
-                selectedVideoSource = value;
                 OnPropertyChanged();
             }
         }
@@ -99,12 +66,13 @@ namespace CVCapturePanel.ViewModel
         {
             get => frame;
 
-            set => SetField(ref frame, value);
+            set
+            {
+                frame = value;
+                OnPropertyChanged();
+            }
         }
-
-        private bool IsDialogOpened { get; set; }
-        private string VideoFilePath { get; set; }
-
+        
         public event ImageWithDetectionChangedEventHandler ImageWithDetectionChanged;
 
         private void InitializeWebCamService()
@@ -120,26 +88,8 @@ namespace CVCapturePanel.ViewModel
 
         private void OnCameraImageChanged(object sender, Image<Bgr, byte> image)
         {
-            //RaiseImageWithDetectionChangedEvent(image);
+            RaiseImageWithDetectionChangedEvent(image);
             Frame = image.Bitmap;
-        }
-
-        private void FillComboBox()
-        {
-            sourceList.Add(new VideoSource("Video"));
-            sourceList.Add(new VideoSource("Camera capture"));
-            Video = new CollectionView(sourceList);
-        }
-
-        private void InitializeServices()
-        {
-            videoPlayingService = new VideoPlayingService();
-            videoPlayingService.VideoFramesChangeEvent += VideoPlayingServiceVideoFramesChangeEvent;
-        }
-
-        private void VideoPlayingServiceVideoFramesChangeEvent(object sender, Mat frame)
-        {
-            Frame = frame.Bitmap;
         }
 
         /// <summary>
@@ -148,8 +98,7 @@ namespace CVCapturePanel.ViewModel
         private void InitializeCommands()
         {
             ToggleWebServiceCommand = new RelayCommand(TogglePlayerServiceExecute);
-            ToogleOpenVideoCommand = new RelayCommand(ToogleOpenVideo);
-            ToogleCloseAppCommand = new RelayCommand(ToogleCloseApp);
+            ToggleCloseAppCommand = new RelayCommand(ToggleCloseApp);
         }
 
         /// <summary>
@@ -157,48 +106,25 @@ namespace CVCapturePanel.ViewModel
         /// </summary>
         private void TogglePlayerServiceExecute()
         {
-            if (SelectedVideoSource == sourceList[1].Name && !IsDialogOpened)
+            if (webCamService == null)
             {
-                if (webCamService == null)
-                {
-                    InitializeWebCamService();
-                }
-
-                if (!webCamService.IsRunning)
-                {
-                    IsStreaming = true;
-                    ButtonContent = "Stop";
-                    webCamService.RunServiceAsync();
-                }
-                else
-                {
-                    IsStreaming = false;
-                    ButtonContent = "Start";
-                    webCamService.CancelServiceAsync();
-                    ClearFrame();
-                    webCamService.Dispose();
-                    webCamService = null;
-                }
+                InitializeWebCamService();
             }
-            else if (SelectedVideoSource == sourceList[0].Name && IsDialogOpened)
+
+            if (!webCamService.IsRunning)
             {
-                if (!videoPlayingService.IsPlaying)
-                {
-                    IsStreaming = true;
-                    ButtonContent = "Stop";
-                    //ToogleOpenVideo();
-                    videoPlayingService.PlayVideo(VideoFilePath);
-                }
-                else
-                {
-                    IsStreaming = false;
-                    ButtonContent = "Start";
-                    videoPlayingService.VideoFramesChangeEvent -= VideoPlayingServiceVideoFramesChangeEvent;
-                    videoPlayingService.Dispose();
-                    videoPlayingService.StopPlaying();
-                    IsDialogOpened = false;
-                    ClearFrame();
-                }
+                IsStreaming = true;
+                ButtonContent = "Stop";
+                webCamService.RunServiceAsync();
+            }
+            else
+            {
+                IsStreaming = false;
+                ButtonContent = "Start";
+                webCamService.CancelServiceAsync();
+                ClearFrame();
+                webCamService.Dispose();
+                webCamService = null;
             }
         }
 
@@ -209,21 +135,11 @@ namespace CVCapturePanel.ViewModel
                 Frame = null;
             }
         }
-
-        private void ToogleOpenVideo()
-        {
-            IsDialogOpened = dialog.OpenFileDialog();
-            VideoFilePath = dialog.FilePath;
-        }
-
-        private void ToogleCloseApp()
+        
+        private void ToggleCloseApp()
         {
             CloseAction?.Invoke();
-            if (webCamService != null)
-            {
-                webCamService.Dispose();
-                videoPlayingService.Dispose();
-            }
+            webCamService?.Dispose();
         }
     }
 }
