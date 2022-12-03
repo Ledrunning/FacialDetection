823b11dc1eab54fb61cd945d0db087f6dfa7181f
Ledrunning <ledrunner@hotmail.com>
2022-12-03 23:41:27 +0100

    Changed name of solution to FaceDetectionApp.sln and project to FaceDetection.csproj

---
+++ FaceDetectionApp/Constants/FaceDetectionConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+
+
+namespace CVCapturePanel.Constants
+{
+    internal static class FaceDetectionConstants
+    {
+        public const int RectangleThickness = 3;
+        public static readonly Color RectangleColor = Color.BurlyWood;
+    }
+}
